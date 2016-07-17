5f2aa655cd4bb68fc2a899cc23e21f34c022dc6c
changhoon-kim <changhoon.kim@worksmobile.com>
2016-07-17 15:31:17 +0900

    [algospot boardcover] modify baord type from string to int for memory

--- algospot/boardcover/boardcover.py
+++ algospot/boardcover/boardcover.py
@@ -6,7 +6,7 @@ def can_put(row, col, index):
         check_col = col + block[index][i][1]
         if check_row >= height or check_col >= width:
             return False
-        if board[check_row][check_col] != '.':
+        if board[check_row][check_col] != 0: # '.'
             return False
 
     return True
@@ -15,17 +15,19 @@ def put(row, col, index):
     for i in range(0, 2):
         put_row = row + block[index][i][0]
         put_col = col + block[index][i][1]
-        board[put_row] = ''.join([board[put_row][:put_col], \
-                                  '#', \
-                                  board[put_row][put_col + 1:]])
+        board[put_row][put_col] = 1 # '#'
+        #board[put_row] = ''.join([board[put_row][:put_col], \
+                                  #'#', \
+                                  #board[put_row][put_col + 1:]])
 
 def unput(row, col, index):
     for i in range(0, 2):
         unput_row = row + block[index][i][0]
         unput_col = col + block[index][i][1]
-        board[unput_row] = ''.join([board[unput_row][:unput_col], \
-                                   '.', \
-                                   board[unput_row][unput_col + 1:]])
+        board[unput_row][unput_col] = 0 # '.'
+        #board[unput_row] = ''.join([board[unput_row][:unput_col], \
+                                   #'.', \
+                                   #board[unput_row][unput_col + 1:]])
 
 def count_cover(row, col):
     if col >= width:
@@ -34,7 +36,7 @@ def count_cover(row, col):
     if row >= height:
         return 1
 
-    if board[row][col] != '.':
+    if board[row][col] != 0: # '.'
         return count_cover(row, col + 1)
 
     result = 0
@@ -49,7 +51,7 @@ def count_cover(row, col):
 def board_cover():
     for i in range(0, height):
         for j in range(0, width):
-            if board[i][j] == '.':
+            if board[i][j] == 0: # '.'
                 return count_cover(i, j)
 
     return 0
@@ -64,7 +66,13 @@ if __name__ == "__main__":
         width = int(width)
         board = []
         for i in range(0, height):
-            board.append(raw_input())
+            line = []
+            for char in raw_input():
+                if char == '.':
+                    line.append(0)
+                else:
+                    line.append(1)
+            board.append(line)
 
         print board_cover()
 
